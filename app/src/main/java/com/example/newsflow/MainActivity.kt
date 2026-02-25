4f0a48a811ebf52b22a7daf770fcd689e3b16610
mohammad <mohammad.litle@gmail.com>
2026-02-25 11:35:58 +0330

    feat: add dark mode support

--- app/src/main/java/com/example/newsflow/MainActivity.kt
+++ app/src/main/java/com/example/newsflow/MainActivity.kt
@@ -4,17 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.example.newsflow.data.local.datastore.SettingDataStore
 import com.example.newsflow.navigation.AppNavGraph
 import com.example.newsflow.ui.theme.NewsFlowTheme
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @Inject
+    lateinit var settingDataStore: SettingDataStore
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            NewsFlowTheme {
+            val isDarkMode by settingDataStore.isDarkMode()
+                .collectAsStateWithLifecycle(initialValue = false)
+
+            NewsFlowTheme(darkTheme = isDarkMode) {
                 AppNavGraph()
             }
         }
